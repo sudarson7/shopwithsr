f277b6f88c3ffd6daf90f8b3516425d2551a49a9
Sudarson Raja <sudarsonraja8@gmail.com>
2026-06-13 15:46:06 +0530

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,32 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+let container = document.getElementById("cartContainer");
+let totalText = document.getElementById("total");
+
+function loadCart() {
+    container.innerHTML = "";
+    let total = 0;
+
+    cart.forEach((p, i) => {
+        total += p.price;
+
+        let div = document.createElement("div");
+        div.innerHTML = `
+            <h3>${p.name}</h3>
+            <p>₹${p.price}</p>
+            <button onclick="removeItem(${i})">Remove</button>
+        `;
+
+        container.appendChild(div);
+    });
+
+    totalText.innerText = "Total ₹" + total;
+}
+
+function removeItem(i) {
+    cart.splice(i, 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    loadCart();
+}
+
+loadCart();
