281840e910c7b8272bb6ba81a551b3e0ae988bee
Sudarson Raja <sudarsonraja8@gmail.com>
2026-06-13 15:46:34 +0530

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -1,1 +1,27 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+let total = 0;
+
+cart.forEach(item => {
+    total += item.price;
+});
+
+document.getElementById("totalAmount").innerText = "Total: ₹" + total;
+
+function placeOrder() {
+
+    let name = document.getElementById("name").value;
+    let address = document.getElementById("address").value;
+    let phone = document.getElementById("phone").value;
+
+    if (name === "" || address === "" || phone === "") {
+        alert("Please fill all details");
+        return;
+    }
+
+    localStorage.removeItem("cart");
+
+    document.getElementById("msg").innerText =
+        "🎉 Order Placed Successfully! Thank you " + name;
+
+}
