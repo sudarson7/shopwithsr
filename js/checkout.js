4ba953fde70de176e2136391e2e10d4e58649a22
Sudarson Raja <sudarsonraja8@gmail.com>
2026-06-14 13:06:34 +0530

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -43,6 +43,7 @@ function placeOrder() {
 
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
+    console.log("Email entered",email);
     let address = document.getElementById("address").value;
     let phone = document.getElementById("phone").value;
 
